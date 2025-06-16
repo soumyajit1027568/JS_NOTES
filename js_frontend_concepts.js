b4c771258d119e5bedd3c9c0d6fcabd78deb872c
Soumyajit Bag <soumyajitbag2001@gmail.com>
2025-06-17 01:55:35 +0530

    async await and this

--- js_frontend_concepts.js
+++ js_frontend_concepts.js
@@ -14,7 +14,7 @@
 // setTimeout(() => {
 //     console.log(i);
 // },1000);
-
+"use strict";
 //**************Capturing and Bubbling*****************
 
 // In the capturing phase, the event starts from the root and goes down to the target element.
@@ -189,3 +189,133 @@ console.log(ob2.getName());
 // │ Use Case             │ Storing data that should     │ Temporary data like form     │
 // │                      │ persist across sessions      │ inputs, filters, etc.        │
 // └──────────────────────┴──────────────────────────────┴──────────────────────────────┘
+
+// ********************map(),filter(),reduce()********************
+
+let arr = [1,3,4,2,5,4,5]
+
+let arrAfterMap = arr.map((e)=>{
+    return e*2;
+})
+console.log("arrAfterMap",arrAfterMap);
+
+let arrAfterReduce = arr.reduce((acc,e)=>{
+    acc = acc+ e;
+    return acc;
+},0)
+console.log("arrAfterReduce",arrAfterReduce);
+
+let arrAfterFilter = arr.filter((e)=>{
+ return e >= 5   
+})
+console.log("arrAfterFilter",arrAfterFilter);
+
+// ********************async and await********************
+
+// total time = 10 seconds, not 15.
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Promise resolved value by p1!!");
+    resolve("Promise resolved value by p1!!");
+  }, 5000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Promise resolved value by p2!!");
+    resolve("Promise resolved value by p2!!");
+  }, 10000);
+});
+async function handlePromise() {
+  console.log("Hi");
+//   debugger;
+  const val = await p1;
+  console.log("Hello There!");
+//   debugger;
+  console.log(val);
+  const val2 = await p2;
+  console.log("Hello There! 2");
+//   debugger;
+  console.log(val2);
+}
+handlePromise();
+//*** p1 and p2 start at the same time, because they are created before the await. await suspends the async function — but it doesn't pause other parts of the code that already ran (like timers).
+// So total time = 10 seconds, not 15.
+
+//even if handlePromise() is never called — both p1 and p2 will still resolve.p1 and p2 don’t care about that suspension. They were already running because the Promises were created outside the async function.These promises are created immediately when the script runs.The setTimeout() callbacks are scheduled immediately.Their timers start immediately, regardless of whether you ever await or .then() on them.
+
+// here if i create promise inside async function.
+// it will take 5 + 10 = 15 sec
+
+// async function handlePromise() {  
+//   console.log("Hi");
+//   const p1 = new Promise(...); // starts at this point
+//   const val = await p1;
+//   const p2 = new Promise(...); // p2 starts only after p1 resolves
+//   const val2 = await p2;
+// }
+
+// ********************this keyword,this substitution********************
+
+// this in global space
+// this inside a function
+// this in strict mode (this substitution)
+// this value depends on how this is called (window)
+// this inside a object's method
+// this inside arrow function
+// this inside nested arrow function
+// this inside dom
+
+console.log(this) // refers to global object (i.e., window in browsers, global in Node.js). global object differs based on runtime environment.
+
+//this inside a function
+function x(){
+    // the below value depends on strict/non-strict mode
+    console.log(this);
+    // in strict mode - undefined
+    // in non-strict mode - refers to global window object because replaced by globalObject for the rule of 'this substitution' 
+
+    // ***this inside non-strict mode (this substitution) 
+    // -> According to this substitution, if the value of this keyword is null/undefined, it will be replaced by globalObject only in non-strict mode. This is the reason why this refers to global window object inside function in non-strict mode
+}
+x(); 
+// ***this keyword value depends on how the function is called.
+// For eg: In strict mode: if i call using a window reference it will not return undefined will return undefined
+window.x(); // window object
+
+//this inside object's method
+const object1 = {
+    a:10,
+    y:function(){
+        console.log(this);
+    }
+}
+object1.y() //if i call with a reference of object return object1 {a: 10, x: ƒ}
+
+//this inside arrow function
+
+//this inside arrow functionArrow function doesn't have their own this value, they take the value from enclosing lexical context
+const object2 = {
+    a:10,
+    y:()=>{
+        console.log("inside arrow fun",this);
+    }
+}
+object2.y(); //return window obj , take the value from enclosing lexical context
+
+const object3 = {
+    a:10,
+    y:function(){
+        console.log("enclosing lexical context",this);
+        const y2 = ()=>{
+            console.log("inside arrow fun but inside another funtion",this); 
+            // in line 301 it will retun window thats why it also returning window that is presnt inside enclosing lexical context
+        }
+        y2()
+    }
+}
+object3.y();
+
+// this inside DOMIt refers to HTML element
+// It refers to HTML element.
+
+// <buttononclick="alert(this)">Click Me</button> --> [object HTMLButtonElement] - Button element  
