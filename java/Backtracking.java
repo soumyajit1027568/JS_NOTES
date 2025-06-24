c332cc67991f67daf1b2c0db450a115cefad843a
Soumyajit Bag <soumyajit.bag@techmahindra.com>
2025-06-24 10:50:56 +0530

    changes

---
+++ java/Backtracking.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Backtracking {
+    //print n to 1 using backtracking
+    public void nToOne(int i,int n) {
+        if(n < i){
+            return;
+        }
+        nToOne(i+1,n);
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        Backtracking sol = new Backtracking();
+        int n = 4;
+        int i =1;
+        sol.nToOne(i,n);
+    }
+}
